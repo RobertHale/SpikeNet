808832dee82af70ab4dd2d4287d6a012d40afc74
RobertHale <rthspike@gmail.com>
2022-01-20 21:08:15 -0600

    add xss prevention

--- webapp/main.go
+++ webapp/main.go
@@ -22,10 +22,10 @@ func main() {
 	// http security headers
 	rootRouter.Use(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			//w.Header().Set("X-Frame-Options", "SAMEORIGIN")
-			//w.Header().Set("X-XSS-Protection", "1; mode=block")
-			//w.Header().Set("Content-Security-Policy", "default-src 'self'; object-src 'none'; style-src 'unsafe-inline' 'self'; img-src 'self' data:")
-			//w.Header().Set("Strict-Transport-Security", "max-age=31536000")
+			w.Header().Set("X-Frame-Options", "SAMEORIGIN")
+			w.Header().Set("X-XSS-Protection", "1; mode=block")
+			w.Header().Set("Content-Security-Policy", "default-src 'self'; object-src 'none'; style-src 'unsafe-inline' 'self'; img-src 'self' data:")
+			w.Header().Set("Strict-Transport-Security", "max-age=31536000")
 
 			next.ServeHTTP(w, r)
 		})
